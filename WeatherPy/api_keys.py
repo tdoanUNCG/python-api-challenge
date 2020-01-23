083f52f57ed132f6ec384cd063ac8a03ec374fb4
tdoanUNCG <48730552+tdoanUNCG@users.noreply.github.com>
2020-01-23 18:13:11 -0500

    remove api keys with gitignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "de222d354dfd9fd137ef4e0fe5c6100d"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyA_Yn5p6Gbhrq8r5Jf5zFaA5R0t10mexyI"
