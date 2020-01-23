a5a1af7b41f49896778df1b1465735674b58c0e1
tdoanUNCG <48730552+tdoanUNCG@users.noreply.github.com>
2020-01-23 15:41:31 -0500

    weatherpy update

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "c1871928a706f49542b72db7bd515256"
+
+# Google API Key
+g_key = "AIzaSyA_Yn5p6Gbhrq8r5Jf5zFaA5R0t10mexyI"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "de222d354dfd9fd137ef4e0fe5c6100d"
+
+# Google API Key
+g_key = "AIzaSyA_Yn5p6Gbhrq8r5Jf5zFaA5R0t10mexyI"
